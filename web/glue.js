ab2a68bd85824ee700091e6ca54d1595f8dee901
lopo <lopo@zju.edu.cn>
2025-03-24 19:42:54 +0800

    feat: glue layer

--- web/glue.js
+++ web/glue.js
@@ -8,10 +8,28 @@ const simpleRand = (alphabet = "abcdefghijklmnopqrstuvwxyz", length = 10) => {
     return result;
 }
 
+const createHostElement = () => {
+    const hostEl = document.createElement('div')
+    hostEl.style.position = 'absolute'
+    // hostEl.style.zIndex = '-1'
+    hostEl.style.width = '100%'
+    hostEl.style.height = '100%'
+    return hostEl
+}
+
+/**
+ * @typedef {Map<String, { resolve: (response: unknown) => void, reject: (error: string) => void }>} HandlerPool
+ */
+
 /**
  * interop object
  */
 window['@fl'] = {
+    /**
+     * @type {HandlerPool}
+     */
+    _pool: new Map(),
+
     /**
      * DO NOT modify this since it will be called from dart side.
      *
@@ -21,11 +39,16 @@ window['@fl'] = {
      * @param response {unknown}
      */
     generalHandler(channelName, serialId, error, response) {
+        const handlerKey = `${channelName}:${serialId}`
+        const handler = this._pool.get(handlerKey)
+        if (!handler) throw new Error(`! no handler found for invocation (${channelName}:${serialId})`)
+
         if (error) {
-            console.error(channelName, serialId, error)
+            handler.reject(error)
         } else {
-            console.log(channelName, serialId, response)
+            handler.resolve(response)
         }
+        this._pool.delete(handlerKey)
     },
 
     /**
@@ -38,33 +61,11 @@ window['@fl'] = {
      */
     invokeMethod(channelName, serialId, arguments) {
         // no-op
-    }
-}
-
-class Manager {
-    static #singleton = null
-
-    constructor() {
-        if (Manager.#singleton) {
-            return Manager.#singleton
-        }
-        Manager.#singleton = this
-    }
-
-    #app = null
-
-    _createHostEl() {
-        const hostEl = document.createElement('div')
-        hostEl.style.position = 'absolute'
-        // hostEl.style.zIndex = '-1'
-        hostEl.style.width = '100%'
-        hostEl.style.height = '100%'
-        return hostEl
-    }
+    },
 
     /**
-     *
-     * @returns {Promise<void>}
+     * Setup flutter engine and add a view to dom.
+     * @returns {Promise<view>}
      */
     prelude() {
         const instance = this
@@ -72,26 +73,59 @@ class Manager {
             try {
                 _flutter.loader.load({
                     onEntrypointLoaded: async function onEntrypointLoaded(engineInitializer) {
+                        // initialize engine and app
                         const engine = await engineInitializer.initializeEngine({
                             multiViewEnabled: true, // Enables embedded mode.
                         });
                         const app = await engine.runApp();
-                        instance.#app = app;
 
-                        const el = instance._createHostEl()
+                        // register handler for 'ready' event, which will be called from dart side after page is ready
+                        instance._pool.set('ready:', {resolve, reject})
+                        const el = createHostElement()
                         document.body.append(el)
-                        app.addView({
-                            hostElement: el
-                        })
-
-                        resolve()
+                        app.addView({hostElement: el})
                     }
                 });
             } catch (err) {
                 reject(err)
             }
         })
+    },
+
+    /**
+     * underlying call
+     * @param {string} channel channel name
+     * @param {unknown} payload payload to send
+     */
+    _interact(channel, payload) {
+        return new Promise((resolve, reject) => {
+            const serial = simpleRand()
+            this._pool.set(`${channel}:${serial}`, {resolve, reject})
+            this.invokeMethod(channel, serial, payload)
+        })
+    },
+
+    /**
+     * @template {unknown} T
+     * @param {T} message
+     * @return {Promise<T>}
+     */
+    echo(message) {
+        return this._interact('echo', message)
+    },
+
+    /**
+     * 生成文献的分享卡片
+     * @param info data for rendering paper card
+     * @return {Uint8Array} buffer of the image
+     */
+    buildPaperCard(info) {
+        return this._interact('paper', info)
     }
 }
 
-window['@fl'].manager = new Manager()
+// Example:
+//
+// await window['@fl'].prelude()
+// const resp = await window['@fl'].echo('hello')
+// console.assert(resp == 'hello', 'never!')
